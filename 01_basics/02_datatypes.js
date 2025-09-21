ed61ed4fba1ae474ddadcc9568ff8d6a3828ff42
Utkarsh Sachan <145625017+IamUtkarshSachan@users.noreply.github.com>
2025-09-21 06:32:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict "// treat all Js code as newer version
+
+//alert("hello") // we are using node js not browser
+
+let name ="Utkarsh"
+let age =21
+let loggedIn =true
+
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined 
